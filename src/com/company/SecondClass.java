db5af07ce7f035af173193939bc2693038a1c117
bulentingezegeni <bulentingezegeni@gmail.com>
2022-02-11 22:37:29 +0300

    failure

--- src/com/company/SecondClass.java
+++ src/com/company/SecondClass.java
@@ -5,5 +5,7 @@ public class SecondClass {
     public static void main(String[] args) {
         System.out.println("new page second class");
         System.out.println("How are you");
+
+        System.out.println("failure");
     }
 }
